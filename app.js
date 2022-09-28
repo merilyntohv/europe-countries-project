c3bc76ebaa3a42f89f57755ad10c607ae6dec675
KaarelKallas <kaarel.kallas@ametikool.ee>
2022-09-28 15:05:48 +0300

    added api

--- app.js
+++ app.js
@@ -3,3 +3,8 @@ document.addEventListener('click', event => {
     const path = event.target
     console.log(path.getAttribute('name'))
 })
+fetch('https://restcountries.com/v3.1/region/europe')
+    .then((response) => console.log(response.json()))
+    
+ // .then((response) => response.json())
+  //.then((data) => console.log(data));
