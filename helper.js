aa278b260ea812caca698e95d5142825b7184700
ralf <ralf@veebisepad.ee>
2022-09-29 22:44:39 +0300

    Add start btn & helper functions

---
+++ helper.js
@@ -0,0 +1,25 @@
+export const randomNumber = (min, max) => {
+    return Math.floor(Math.random() * (max - min) + min);
+  }
+
+export const getTimeStamp = (d) => new Date(d * 1000).toISOString().slice(11, 19);
+
+export const confettiFire = () => {
+    let duration = 15 * 1000;
+    let animationEnd = Date.now() + duration;
+    let defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+
+    const randomInRange = (min, max) => Math.random() * (max - min) + min;
+  
+    let interval = setInterval(function() {
+      let timeLeft = animationEnd - Date.now();
+
+      if (timeLeft <= 0) {
+        return clearInterval(interval);
+      }
+
+      let particleCount = 50 * (timeLeft / duration);
+      confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 } }));
+      confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 } }));
+    }, 250);
+}
